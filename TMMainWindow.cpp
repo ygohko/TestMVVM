a4071eeb97e4a2e8971a53fcff04262962830a02
ygohko <ygohko@gmail.com>
2022-02-11 13:15:35 +0900

    Clean up (#1)
    * Cleaned up.
    
    * Cleaned up.
    
    * Removed a unneeded file.

--- TMMainWindow.cpp
+++ TMMainWindow.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2021 Yasuaki Gohko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include "TMMainWindow.h"
 #include "ui_TMMainWindow.h"
 
@@ -12,18 +34,16 @@ TMMainWindow::~TMMainWindow() {
 void TMMainWindow::setViewModel(QObject* viewModel) {
     this->viewModel = viewModel;
     this->connect(ui->calculateButton, SIGNAL(clicked()), viewModel, SLOT(calculate()));
-    this->connect(viewModel, SIGNAL(populateToViewRequested()), this, SLOT(populateToView()));
-    this->connect(viewModel, SIGNAL(populateToViewModelRequested()), this, SLOT(populateToViewModel()));
+    this->connect(viewModel, SIGNAL(populationToViewRequested(int)), this, SLOT(populateToView(int)));
+    this->connect(viewModel, SIGNAL(populationToViewModelRequested(int)), this, SLOT(populateToViewModel(int)));
 }
 
-void TMMainWindow::populateToView() {
-    /// @todo Implement this
-    ui->priceLineEdit->setText(QString("%1").arg(this->viewModel->property("price").toInt()));
+void TMMainWindow::populateToView(int hint) {
+    Q_UNUSED(hint);
     ui->totalLineEdit->setText(QString("%1").arg(this->viewModel->property("total").toInt()));
 }
 
-void TMMainWindow::populateToViewModel() {
-    /// @todo Implement this
+void TMMainWindow::populateToViewModel(int hint) {
+    Q_UNUSED(hint);
     this->viewModel->setProperty("price", ui->priceLineEdit->text().toInt());
-    this->viewModel->setProperty("total", ui->totalLineEdit->text().toInt());
 }
--- TMMainWindowViewModel.h
+++ TMMainWindowViewModel.h
@@ -1,6 +1,29 @@
+/*
+ * Copyright (c) 2021 Yasuaki Gohko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef TMMAINWINDOWVIEWMODEL_H
 #define TMMAINWINDOWVIEWMODEL_H
 
+// Qt includes
 #include <QObject>
 
 class TMMainWindowViewModel : public QObject {
@@ -12,15 +35,16 @@ public:
     Q_PROPERTY(int price MEMBER price);
     Q_PROPERTY(int total MEMBER total);
 
-    int price;
-    int total;
-
 signals:
-    void populateToViewRequested();
-    void populateToViewModelRequested();
+    void populationToViewRequested(int hint);
+    void populationToViewModelRequested(int hint);
 
 public slots:
     void calculate();
+
+private:
+    int price;
+    int total;
 };
 
 #endif
